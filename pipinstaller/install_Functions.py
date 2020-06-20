538d6f27c9c39278706fc04681345548e5b3c937
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-20 18:52:42 -0400

    feat: add `requirements.txt` install function
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- pipinstaller/install_Functions.py
+++ pipinstaller/install_Functions.py
@@ -27,7 +27,7 @@ def checkPip() -> bool:
 def checkPackage(pkg):
     """Function to check PIL install.
 
-    pkg - a list of packages to check for
+    pkg - a list of packages to check for\n
     returns boolean annotating if package is installed or not
     """
     try:
@@ -72,7 +72,7 @@ def installPip(printOut = False):
 def installPackage(pkg, printOut = False):
     """Function to install Package.
     
-    pkg - package to be installed
+    pkg - package to be installed\n
     returns boolean annotating if an error was raised or not
     """
     cmd = "py -3 -m pip install {} --user".format(pkg[1]) if sys.platform == 'win32' else "python3 -m pip install {} --user".format(pkg[1])
@@ -91,6 +91,40 @@ def installPackage(pkg, printOut = False):
             for line in process.stdout:
                 print(line.decode(), end = '')
 
+    noError = True # Assumes no errors have occurred
+    for line in process.stderr:
+        if line.lower().startswith(b"deprecation"):
+            break
+        print("Err: {}".format(line.decode()), end = '')
+        noError = False # If there are any items from stderr, then an error has occurred
+    
+    if not noError:
+        print("Something went wrong in installation, see information above.")
+    # else:
+    #     print(pkg[1],"was successfully installed")
+    return noError
+
+def installRequirements(path = "pipinstaller/resources/requirements.txt", printOut = False):
+    """Installs via requirements.txt
+    
+    Pass in path to requirmenets\n
+    returns boolean annotating if an error was raised or not
+    """
+    cmd = f"py -3 -m pip install -r {path}/requirements.txt --user" if sys.platform == 'win32' else f"python3 -m pip install -r {path}/requirements.txt --user"
+
+     # Runs the pip install -r requirements.txt
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    
+    if not printOut: # Default Behaviour
+        process.wait()
+    else:
+        while process.poll() is None: # Keeps printing stdout until the process is done
+            for line in process.stdout:
+                print(line.decode(), end = '')
+    
     noError = True # Assumes no errors have occurred
     for line in process.stderr:
         if line.lower().startswith(b"deprecation"):
