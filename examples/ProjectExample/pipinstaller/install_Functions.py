6dd89a087ec8bf3617000b02c18e2d45aefcb43f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-06-20 21:21:31 -0400

    refactor: format code with psf/black
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- examples/ProjectExample/pipinstaller/install_Functions.py
+++ examples/ProjectExample/pipinstaller/install_Functions.py
@@ -10,18 +10,20 @@ import pathlib
 packagesMissing = []
 resPath = pathlib.Path("pipinstaller/resources")
 
+
 def checkPip() -> bool:
     """Function to check pip install"""
-    cmd = "py -3 -m pip" if sys.platform == 'win32' else "python3 -m pip" #This command will normally give options for 
-                                  #arguments for the pip command but will output to stderr if the command is not found
-    process = subprocess.Popen(cmd,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    cmd = (
+        "py -3 -m pip" if sys.platform == "win32" else "python3 -m pip"
+    )  # This command will normally give options for
+    # arguments for the pip command but will output to stderr if the command is not found
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
     for _ in process.stderr:
         return False
-    return True #Returns if whether it is installed or not
+    return True  # Returns if whether it is installed or not
 
 
 def checkPackage(pkg):
@@ -32,72 +34,82 @@ def checkPackage(pkg):
     """
     try:
         _ = __import__(pkg[0])
-    except ImportError: # If the package in question is not installed, this import will raise an ImportError
+    except ImportError:  # If the package in question is not installed, this import will raise an ImportError
         packagesMissing.append(pkg)
         return False
     print(pkg[0], "was found on the system")
-    return True # Returns if whether it is installed or not
+    return True  # Returns if whether it is installed or not
 
 
-def installPip(printOut = False):
+def installPip(printOut=False):
     """Function to install pip.
 
     returns boolean annotating if an error was raised or not
     """
-    cmd = "py -3 pipinstaller\\resources\get-pip.py --user" if sys.platform == 'win32' else "python3 pipinstaller/resources/get-pip.py --user" 
+    cmd = (
+        "py -3 pipinstaller\\resources\get-pip.py --user"
+        if sys.platform == "win32"
+        else "python3 pipinstaller/resources/get-pip.py --user"
+    )
     # Runs the python script in resources and installs pip locally for the user
-    process = subprocess.Popen(cmd,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    if not printOut: # Default Behaviour
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    if not printOut:  # Default Behaviour
         process.wait()
     else:
-        while process.poll() is None: # Keeps printing stdout until the process is done
+        while process.poll() is None:  # Keeps printing stdout until the process is done
             for line in process.stdout:
-                print(line.decode(), end = '')
+                print(line.decode(), end="")
 
-    noError = True # Assumes no errors have occurred
+    noError = True  # Assumes no errors have occurred
     for line in process.stderr:
         if line.lower().startswith(b"deprecation"):
             break
-        print("Err: {}".format(line.decode()), end = '')
-        noError = False # If there are any items from stderr, then an error has occurred
+        print("Err: {}".format(line.decode()), end="")
+        noError = (
+            False  # If there are any items from stderr, then an error has occurred
+        )
 
     if not noError:
         print("Something went wrong in installation, see information above.")
     return noError
 
 
-def installPackage(pkg, printOut = False):
+def installPackage(pkg, printOut=False):
     """Function to install Package.
     
     pkg - package to be installed
     returns boolean annotating if an error was raised or not
     """
-    cmd = "py -3 -m pip install {} --user".format(pkg[1]) if sys.platform == 'win32' else "python3 -m pip install {} --user".format(pkg[1])
-    #Runs the pip command to install the Pillow (PIL) package from online
-    # cmd = "py -3 -m pip install resources\Pillow-4.3.0-cp33-cp33m-win32.whl --user" if sys.platform == 'win32' else "python3 -m pip install Pillow" 
+    cmd = (
+        "py -3 -m pip install {} --user".format(pkg[1])
+        if sys.platform == "win32"
+        else "python3 -m pip install {} --user".format(pkg[1])
+    )
+    # Runs the pip command to install the Pillow (PIL) package from online
+    # cmd = "py -3 -m pip install resources\Pillow-4.3.0-cp33-cp33m-win32.whl --user" if sys.platform == 'win32' else "python3 -m pip install Pillow"
     # Runs the pip command for the wheel file in resources
-    process = subprocess.Popen(cmd,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
-    if not printOut: # Default Behaviour
+    if not printOut:  # Default Behaviour
         process.wait()
     else:
-        while process.poll() is None: # Keeps printing stdout until the process is done
+        while process.poll() is None:  # Keeps printing stdout until the process is done
             for line in process.stdout:
-                print(line.decode(), end = '')
+                print(line.decode(), end="")
 
-    noError = True # Assumes no errors have occurred
+    noError = True  # Assumes no errors have occurred
     for line in process.stderr:
         if line.lower().startswith(b"deprecation"):
             break
-        print("Err: {}".format(line.decode()), end = '')
-        noError = False # If there are any items from stderr, then an error has occurred
-    
+        print("Err: {}".format(line.decode()), end="")
+        noError = (
+            False  # If there are any items from stderr, then an error has occurred
+        )
+
     if not noError:
         print("Something went wrong in installation, see information above.")
     # else:
--- pipinstaller/install_packages.py
+++ pipinstaller/install_packages.py
@@ -3,7 +3,14 @@
 Copyright 2020 Misha Melnyk, Luke Zhang | MIT License
 """
 
-from pipinstaller.install_Functions import checkPip, installPip, checkPackage, installPackage, packagesMissing, installRequirements
+from pipinstaller.install_Functions import (
+    checkPip,
+    installPip,
+    checkPackage,
+    installPackage,
+    packagesMissing,
+    installRequirements,
+)
 import sys
 
 targetPackages = [
@@ -11,27 +18,31 @@ targetPackages = [
 ]
 
 
-def install(pathToRequirements = "pipinstaller/resources"):
+def install(pathToRequirements="pipinstaller/resources"):
     """Installs the specified packages\n
     Leave this blank if you are using targetPackages, or if your requirements.txt matches this path
     """
     global targetPackages
 
-    acceptAll = "-y" in sys.argv # Checks if the -y parameter is passed in. Will ignore [y/n] queries if it is
+    acceptAll = (
+        "-y" in sys.argv
+    )  # Checks if the -y parameter is passed in. Will ignore [y/n] queries if it is
 
     pipInstalled = checkPip()
 
     ###################################################################################################
     suffix = "/" if pathToRequirements[-1] != "/" else ""
     try:
-        open(f"{pathToRequirements}{suffix}requirements.txt") # Check for a requirements.txt file
+        open(
+            f"{pathToRequirements}{suffix}requirements.txt"
+        )  # Check for a requirements.txt file
 
         if acceptAll:
             doFullLog = True
         else:
             query = input("Show full output log? [y/n] ")
-            doFullLog = True if query.lower() in ["y", 'yes'] else False
-        
+            doFullLog = True if query.lower() in ["y", "yes"] else False
+
         installRequirements(pathToRequirements, printOut=doFullLog)
 
         return
@@ -39,11 +50,13 @@ def install(pathToRequirements = "pipinstaller/resources"):
         pass
 
     ###################################################################################################
-    if len(targetPackages) == 0: # If target packages array is not speficied
-        with open("pipinstaller/resources/targetPackages.txt") as fin: # Read from targetpackages.txt
+    if len(targetPackages) == 0:  # If target packages array is not speficied
+        with open(
+            "pipinstaller/resources/targetPackages.txt"
+        ) as fin:  # Read from targetpackages.txt
             targetPackages = fin.read().splitlines()
             for i in range(len(targetPackages)):
-                targetPackages[i] = targetPackages[i].split(',')
+                targetPackages[i] = targetPackages[i].split(",")
     for pkg in targetPackages:
         checkPackage(pkg)
     if len(packagesMissing) == 0:
@@ -54,36 +67,38 @@ def install(pathToRequirements = "pipinstaller/resources"):
         doFullLog = True
     else:
         query = input("Show full output log? [y/n] ")
-        doFullLog = True if query.lower() in ["y", 'yes'] else False
+        doFullLog = True if query.lower() in ["y", "yes"] else False
     ###################################################################################################
-    if pipInstalled and len(packagesMissing) == 0: # If both pip and PIL are installed
+    if pipInstalled and len(packagesMissing) == 0:  # If both pip and PIL are installed
         print("Pip and all packages are installed.\nNo installations are required.")
         return
-    ###################################################################################################   
-    if not pipInstalled: # If pip is not installed
+    ###################################################################################################
+    if not pipInstalled:  # If pip is not installed
         print("Pip was not found on the system.")
         if acceptAll:
             answer = "y"
         else:
             answer = input("Would you like to install the pip command? [y/n] ")
-        
+
         if answer.lower() in ["y", "yes"]:
             print("Preparing to install Pip...")
             pipSuccess = installPip(doFullLog)
-            
+
             if not pipSuccess:
                 print("Error occurred when installing pip")
                 if not acceptAll:
-                    if not input("Would you like to try to continue anyways?[y/n] ").lower() in ["y", "yes"]:
+                    if not input(
+                        "Would you like to try to continue anyways?[y/n] "
+                    ).lower() in ["y", "yes"]:
                         return
 
             else:
                 print("Pip was installed successfully!")
-        
+
         else:
             print("Package installation cannot continue without pip.\nExiting...")
             return
-###################################################################################################
+    ###################################################################################################
     for pkg in packagesMissing:
         print(f"Package {pkg[0]} was not found on the system.")
         if acceptAll:
@@ -94,13 +109,17 @@ def install(pathToRequirements = "pipinstaller/resources"):
         if answer.lower() in ["y", "yes"]:
             print(f"Preparing to install {pkg[0]}...")
             pkgSuccess = installPackage(pkg, doFullLog)
-            
+
             if not pkgSuccess:
                 print(f"Error occurred when installing {pkg[0]}")
-                if not acceptAll and not input("Would you like to try to continue anyways?[y/n] ").lower() in ["y", "yes"]:
+                if not acceptAll and not input(
+                    "Would you like to try to continue anyways?[y/n] "
+                ).lower() in ["y", "yes"]:
                     return
             else:
                 print(f"{pkg[0]} was installed successfully!")
-    
+
     print("Everything Finished! Exiting...")
+
+
 ###################################################################################################
